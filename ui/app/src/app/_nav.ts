412c4acde48a9a0c403a950f55fbc1ab6222a249
Michael Bironneau <michael.bironneau@openenergi.com>
2018-01-26 17:20:16 +0000

    Remove UI files to put in separate repo so it doesn't interfere with IDE and Goconvey

--- ui/app/src/app/_nav.ts
+++
@@ -1,7 +0,0 @@
-export const navigation = [
-  {
-    name: 'Editor',
-    url: '/dashboard',
-    icon: 'icon-pencil'
-  }
-];
--- ui/app/src/app/directives/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './aside';
-export * from './nav-dropdown';
-export * from './replace';
-export * from './sidebar';
-export * from './codemirror';
