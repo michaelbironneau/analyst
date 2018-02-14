0848fc44e99ec9b70cd8e7766e05b4a268685df9
tappoz <tappoz84@yahoo.it>
2018-02-14 14:01:32 +0000

    The Reduce phase on the Min aggregate handles timestamps in the expected formats

--- transforms/min_test.go
+++ transforms/min_test.go
@@ -1,8 +1,10 @@
 package transforms
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
+	"math"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestMin(t *testing.T) {
@@ -25,6 +27,40 @@ func TestMin(t *testing.T) {
 			f := agg.Return()
 			So(f, ShouldNotBeNil)
 			So(*f, ShouldEqual, -0.1)
+		})
+		Convey("It should reduce multiple timestamps correctly", func() {
+			expectedMin := "2018-02-13T01:00:00Z"
+			msgs := [][]interface{}{
+				[]interface{}{"2018-02-14T10:00:00Z"},
+				[]interface{}{"2018-02-14T05:00:00Z"},
+				[]interface{}{expectedMin},
+				[]interface{}{"2018-02-14T03:00:00Z"},
+				[]interface{}{"2018-02-14T05:30:00Z"},
+				[]interface{}{"2018-02-14T05:00:30Z"},
+			}
+
+			// the following makes sure the defaults are as expected,
+			// this is waiting for a deeper fix to be performed in the
+			// default values for the "min" structure
+			agg.result = math.MaxFloat64
+
+			for _, msg := range msgs {
+				err := agg.Reduce(msg)
+				So(err, ShouldBeNil)
+			}
+			f := agg.Return()
+			So(f, ShouldNotBeNil)
+			expectedMinTimestamp, _, err := parseTime(expectedMin)
+			So(err, ShouldBeNil)
+			So(*f, ShouldEqual, float64(expectedMinTimestamp.Unix()))
+		})
+		Convey("It should raise an error when a string is not in the expected timestamp formats", func() {
+			expectedRejectionTimestampFormat := "FOO_BAR_BAZ"
+			err := agg.Reduce([]interface{}{expectedRejectionTimestampFormat})
+			So(err, ShouldBeError)
+			So(err.Error(), ShouldEqual, "unknown time format FOO_BAR_BAZ: expected RFC3339, RFC3339 with nanoseconds or YYYY-MM-DDTHH:MM:SSZ")
+			f := agg.Return()
+			So(*f, ShouldBeZeroValue)
 		})
 	})
 }
